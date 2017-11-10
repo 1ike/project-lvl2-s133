06a209ebfb1833fdce3fe305ac6b899e294cff01
discodancer <googlohren@gmail.com>
2017-11-10 14:19:49 +0300

    Rerefix test for cc.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,12 +4,15 @@ import os from 'os';
 import gendiff from '../src';
 
 const dir = __dirname + '/__fixtures__';
+const prefixBefore = dir + '/before.';
+const prefixAfter = dir + '/after.';
+
 const expected = fs.readFileSync(dir + '/expected.txt', 'utf8');
 
 test('gendiff json', () => {
   const inputFormat = 'json';
-  const pathToFile1 = dir + '/before.'+ inputFormat;
-  const pathToFile2 = dir + '/after.' + inputFormat;
+  const pathToFile1 = prefixBefore + inputFormat;
+  const pathToFile2 = prefixAfter + inputFormat;
 
   const current = gendiff(pathToFile1, pathToFile2, inputFormat);
 
@@ -20,8 +23,8 @@ test('gendiff json', () => {
 test('gendiff yml', () => {
   const inputFormatFull = 'yaml';
   const inputFormatShort = 'yml';
-  const pathToFile1 = dir + '/before.yml';
-  const pathToFile2 = dir + '/after.yml';
+  const pathToFile1 = prefixBefore + inputFormatShort;
+  const pathToFile2 = prefixAfter + inputFormatShort;
 
   const currentFull = gendiff(pathToFile1, pathToFile2, inputFormatFull);
   const currentShort = gendiff(pathToFile1, pathToFile2, inputFormatShort);
@@ -33,8 +36,8 @@ test('gendiff yml', () => {
 
 test('gendiff ini', () => {
   const inputFormat = 'ini';
-  const pathToFile1 = dir + '/before.' + inputFormat;
-  const pathToFile2 = dir + '/after.' + inputFormat;
+  const pathToFile1 = prefixBefore + inputFormat;
+  const pathToFile2 = prefixAfter + inputFormat;
 
   const current = gendiff(pathToFile1, pathToFile2, inputFormat);
 
