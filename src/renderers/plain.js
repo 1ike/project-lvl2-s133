b023707953824f574e662f6dc27f6c80c9082101
discodancer <googlohren@gmail.com>
2017-11-12 01:01:46 +0300

    Clean plain render from old functions.

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -1,65 +1,6 @@
 import os from 'os';
 
-import { getTypeForShow, typeEnums as t } from '../libs';
-
-
-const isActualLeaf = (type, hasChildren) => type === t.actual && !hasChildren;
-
-const getNewItemNode = (type, hasChildren, item) => {
-  const isNotActual = hasChildren && type !== t.actual;
-  return isNotActual ? item : [];
-};
-
-const getResultItem = (hasChildren, item, newItemNode) => {
-  const res = hasChildren ? newItemNode : item;
-  return res;
-};
-
-const flatten = (ast) => {
-  if (!Array.isArray(ast)) return [];
-
-  const levelDiff = ast.reduce((acc, item) => {
-    const { value, types } = item;
-
-    const hasChildren = Array.isArray(value);
-
-    const type = getTypeForShow(types);
-
-    if (isActualLeaf(type, hasChildren)) return acc;
-
-    const newItemNode = getNewItemNode(type, hasChildren, item);
-
-    const resultItem = getResultItem(hasChildren, item, newItemNode);
-
-
-    return acc.concat(resultItem, flatten(value));
-  }, []);
-
-  return levelDiff;
-};
-
-
-const prepareValue = (value, type) => {
-  const newValue = typeof value === 'string' ? `'${value}'` : value;
-  if (type === t.added) {
-    return typeof newValue === 'object' ? 'complex value' : `value: ${newValue}`;
-  }
-
-  return typeof value === 'string' ? `'${value}'` : value;
-};
-
-const getChanges = (value, valueOld, type) => {
-  const newValue = prepareValue(value, type);
-  const newValueOld = prepareValue(valueOld, type);
-  if (type === t.updated) {
-    return `updated. From ${newValueOld} to ${newValue}`;
-  }
-  if (type === t.added) {
-    return `added with ${newValue}`;
-  }
-
-  return t.removed;
-};
+import t from '../libs';
 
 
 const render = (ast, path = []) => {
