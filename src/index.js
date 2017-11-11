5138b3b582497edd7e8e578c06fecdc1b7f95345
discodancer <googlohren@gmail.com>
2017-11-11 10:28:18 +0300

    Delete unnecessary dependencies.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import yamlParser from 'js-yaml';
 import ini from 'ini';
 
 import getAST from './ast';
-import toJSONString from './json';
+import toTreeString from './tree';
 import toPlainString from './plain';
 
 const parsers = {
@@ -15,11 +15,11 @@ const parsers = {
 };
 
 const transformersToString = {
-  json: toJSONString,
+  tree: toTreeString,
   plain: toPlainString,
 };
 
-const gendiff = (pathToFile1, pathToFile2, inputFormat, outputFormat = 'json') => {
+const gendiff = (pathToFile1, pathToFile2, inputFormat, outputFormat = 'tree') => {
   const file1 = fs.readFileSync(pathToFile1, 'utf8');
   const file2 = fs.readFileSync(pathToFile2, 'utf8');
 
